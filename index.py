937e9468353bd7fa5d0e28035dd89d85d400a426
kspace-trk <keigonakao.0825@gmail.com>
2023-07-24 23:48:06 +0900

    joystickの操作完全に理解した

--- index.py
+++ index.py
@@ -1,7 +1,29 @@
 import pyvjoy
+import time
 
 j = pyvjoy.VJoyDevice(1)
 
-j.set_axis(pyvjoy.HID_USAGE_X, 0x4000)
+# ジョイスティックの軸を元に戻す（中央に配置する）
+j.data.wAxisX = 0x4000
+j.data.wAxisY = 0x4000
 
-j.reset_povs()
+
+
+# for i in range(0,10000,1):
+# ジョイスティックのX軸を前に倒す（前進する）
+j.data.wAxisX = 0x8000
+
+# vJoyデバイスにデータを送信
+j.update()
+print('updated')
+
+# 3秒間待つ
+time.sleep(3)
+
+# ジョイスティックの軸を元に戻す（中央に配置する）
+j.data.wAxisX = 0x4000
+j.data.wAxisY = 0x4000
+
+# vJoyデバイスにデータを送信
+j.update()
+
