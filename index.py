d89cd78b61dde0a53d68d94a0f8a421bac9b574c
kspace-trk <keigonakao.0825@gmail.com>
2023-07-25 00:49:24 +0900

    wAxisZ指定すれば、右スティックを真ん中にできる

--- index.py
+++ index.py
@@ -6,16 +6,17 @@ j = pyvjoy.VJoyDevice(1)
 # ジョイスティックの軸を元に戻す（中央に配置する）
 j.data.wAxisX = 0x4000
 j.data.wAxisY = 0x4000
+j.data.wAxisZ = 0x4000
 
 
-
-# for i in range(0,10000,1):
-# ジョイスティックのX軸を前に倒す（前進する）
+# ジョイスティックのX軸を前に倒す
 j.data.wAxisX = 0x8000
+j.data.wAxisY = 0x4000
 
 # vJoyデバイスにデータを送信
 j.update()
 print('updated')
+print(dir(j.data))
 
 # 3秒間待つ
 time.sleep(3)
@@ -23,7 +24,11 @@ time.sleep(3)
 # ジョイスティックの軸を元に戻す（中央に配置する）
 j.data.wAxisX = 0x4000
 j.data.wAxisY = 0x4000
+j.data.wAxisZ = 0x4000
 
 # vJoyデバイスにデータを送信
 j.update()
 
+# 念のためリセット
+j.reset_povs()
+j.reset()
